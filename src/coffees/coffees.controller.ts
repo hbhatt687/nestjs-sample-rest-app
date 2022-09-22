b5037fb2cf10aed514d623e9e3acd1a04a43dc60
Harsh Bhatt <hbhatt687@gmail.com>
2022-09-21 20:34:08 -0400

    add pipes, interceptors, and protocols

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Post, Body, Patch, Delete, Query, SetMetadata } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete, Query, SetMetadata, ParseIntPipe } from '@nestjs/common';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
@@ -11,13 +12,12 @@ export class CoffeesController {
 
     @Public()
     @Get()
-    findAll(@Query() paginationQuery: PaginationQueryDto) {
-        const { limit, offset } = paginationQuery;
+    findAll(@Query() paginationQuery: PaginationQueryDto) {        const { limit, offset } = paginationQuery;
         return this.coffeeService.findAll(paginationQuery);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
+    findOne(@Param('id', ParseIntPipe) id: string) {
         return this.coffeeService.findOne(id);
     }
 
